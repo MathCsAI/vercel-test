e978fa119251a3f40463bd394d05afb743db6070
Abhishek Bhattacharjee <145133199+MathCsAI@users.noreply.github.com>
2026-02-10 17:40:40 +0000

    Disable API response caching

--- api/pipeline.js
+++ api/pipeline.js
@@ -141,6 +141,7 @@ module.exports = async (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Cache-Control", "no-store");
 
   if (req.method === "OPTIONS") {
     res.status(204).end();
